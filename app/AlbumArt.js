687c48639bcd8459a34bda30d17c56ea3aa97791
Alejandro Gonzales <jandog8990@gmail.com>
2020-01-05 14:26:22 -0700

    Added new react native music player for testing m3u8

---
+++ app/AlbumArt.js
@@ -0,0 +1,46 @@
+/**
+ * Album art component for the main music player, contains image and
+ * button actions for opacity and other styles
+ */
+
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+const AlbumArt = ({
+  url,
+  onPress
+}) => (
+  <View style={styles.container}>
+    <TouchableOpacity onPress={onPress}>
+      <Image
+        style={styles.image}
+        source={{uri: url}}
+      />
+    </TouchableOpacity>
+  </View>
+);
+
+export default AlbumArt;
+
+const { width, height } = Dimensions.get('window');
+const imageSize = width - 48;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  image: {
+    width: imageSize,
+    height: imageSize,
+  },
+})
